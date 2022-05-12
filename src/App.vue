9b9df764506f88e5477192b238e1eaa574468155
Jabuf <jason.buffet@gmail.com>
2022-05-12 18:02:12 +0200

    component: add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
-<template>
-  <img alt="Vue logo" src="../public/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UropaNavBar from "@/components/UropaNavBar"
+import UropaFooter from "@/components/UropaFooter"
+import Home from "@/views/Home"
 
 export default {
   name: 'App',
   components: {
+    Home,
+    UropaFooter,
+    UropaNavBar,
     HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <UropaNavBar/>
+  <Home/>
+  <UropaFooter/>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {mdiDotsCircle} from '@mdi/js'
+import ProcessingRecordStepper from '@/components/ProcessingRecordStepper.vue'
+import Icon from '@/components/Icon.vue'
+</script>
+
+<template>
+  <section class="px-0 md:px-6 py-6">
+    <div
+        class="text-center"
+    >
+      <button
+          class="inline-flex items-center"
+          disabled
+          type="button"
+      >
+        <icon
+            :path="mdiDotsCircle"
+            class="animate-spin"
+        />
+        Processing...
+      </button>
+    </div>
+  </section>
+  <section class="px-0 md:px-6 py-6">
+    <ProcessingRecordStepper/>
+  </section>
+</template>
