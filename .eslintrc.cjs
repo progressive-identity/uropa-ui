b08940e492d0b57597b42d7bd05414d7ed24971f
Jabuf <jason.buffet@gmail.com>
2022-05-18 13:17:37 +0200

    config: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "jest": true
-    },
-    "extends": [
-        "airbnb-base",
-        "prettier"
-    ],
-    "plugins": [
-        "prettier",
-        "import",
-        "jest"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2020,
-        "sourceType": "module"
-    },
-    "rules": {
-        "prettier/prettier": "error",
-        "no-param-reassign": "off",
-        "camelcase": "off",
-        "consistent-return": "off",
-        "no-unused-vars": [
-            "error",
-            {
-                "vars": "local",
-                "args": "all",
-                "ignoreRestSiblings": false
-            }
-        ]
-    }
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: ['prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    semi: [2, 'never'],
+    'comma-dangle': ['error', 'never'],
+  }
 }
