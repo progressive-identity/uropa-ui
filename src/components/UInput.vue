4226b9b6af869f8954ab143d5ab6621309000b10
Jabuf <jason.buffet@gmail.com>
2022-05-19 10:55:27 +0200

    component: add props beginning and end

--- src/components/UInput.vue
+++ src/components/UInput.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-      class="relative border border-gray-300 rounded-md rounded-b-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
-    <label for="name" class="block text-xs font-medium text-gray-900">{{label}}</label>
+      class="relative border border-gray-300 px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600"
+      v-bind:class="{'rounded-t-lg': beginning, 'rounded-b-lg': end}">
+    <label for="name" class="block text-xs font-medium text-gray-900">{{ label }}</label>
     <input type="text" name="todo" id="todo" v-model="model"
            class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
            :placeholder="placeholder"/>
@@ -21,6 +22,16 @@ const props = defineProps({
   placeholder: {
     type: String,
     required: false
+  },
+  beginning: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  end: {
+    type: Boolean,
+    required: false,
+    default: false
   }
 })
 </script>
