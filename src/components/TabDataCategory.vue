5dd3052b546261a4a2db8dc681dbab48d3c61a6e
Jabuf <jason.buffet@gmail.com>
2022-05-24 12:50:38 +0200

    component: add components for dataCategories

---
+++ src/components/TabDataCategory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="sm:hidden">
+      <select id="tabs" name="tabs"
+              class="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+        <option v-for="tab in dataCategories" :key="tab.name" :selected="true">{{ tab.name }}</option>
+      </select>
+    </div>
+    <div class="hidden sm:block">
+      <nav class="flex space-x-4" aria-label="Tabs">
+        <div v-for="dataCategory in dataCategories" :key="dataCategory.name"
+             :class="[dataCategory.current ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700', 'px-3 py-2 font-medium text-sm rounded-md']"
+             class="text-gray-900"
+             :aria-current="dataCategory.current ? 'page' : undefined">
+          {{ dataCategory.name }}
+          <!--          <div></div>-->
+          <p v-for="dataType in dataCategory.dataTypes">{{ dataType.name }}
+          </p>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataCategories: {
+      type: Object,
+      required: true
+    },
+    current: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  }
+}
+</script>
--- src/components/form/FormDataCategories.vue
+++ src/components/form/FormDataCategories.vue
@@ -1,17 +1,46 @@
 <template>
-  <div v-if="current === 3" class="space-y-8 divide-y divide-gray-200">
-    <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-      <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-        RECIPIENTS
-      </div>
+  <div v-if="current === 3" class="space-y-8">
+    <StepperDataCategories/>
+    <div class=" py-5">
+      <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="purpose in purposes" :key="purpose.name"
+            class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+          <div class="w-full flex items-center justify-between p-6 space-x-6">
+            <div class="flex-1 truncate">
+              <p class="text-gray-900 text-sm font-medium truncate">Purpose : {{ purpose.name }}</p>
+              <p class="text-gray-900 text-sm font-medium truncate">Legal basis : {{ purpose.legalBasis.type }}</p>
+              <TabDataCategory :data-categories="purpose.dataCategories"/>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
 import {useStore} from '@/store/stepper'
+import {useStoreData} from "@/store/data.js"
 import {storeToRefs} from 'pinia'
+import StepperDataCategories from "@/components/stepper/StepperDataCategories.vue"
+import dataCategories from './../../data/dataCategories.json'
+import TabDataCategory from "@/components/TabDataCategory.vue"
 
 const store = useStore()
 const {current} = storeToRefs(store)
+const {processingRecord: {purposes}} = useStoreData()
+
+purposes[0] = {
+  "name": "whatever",
+  "description": "",
+  "isMain": true,
+  "legalBasis": {
+    "type": "contentment",
+    "description": "",
+    "proofUrl": ""
+  },
+  dataCategories
+}
+
+
 </script>
