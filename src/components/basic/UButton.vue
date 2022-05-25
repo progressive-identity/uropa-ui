3d043f3642a5053688dc15d111a36ed25ba6bb2d
Jabuf <jason.buffet@gmail.com>
2022-05-25 13:21:14 +0200

    component: rework the button component

--- src/components/basic/UButton.vue
+++ src/components/basic/UButton.vue
@@ -1,26 +1,48 @@
 <template>
   <button type="button" v-on:click="actionOnClick"
-          class="inline-flex items-center px-3 py-3 border border-transparent shadow-sm text-white font-medium rounded-md text-gray-700 bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          :class="classes">
     <UIcon v-if="icon" :path="icon"/>
     {{ label }}
   </button>
 </template>
 
-<script setup>
+<script>
 import UIcon from "@/components/basic/UIcon.vue"
 
-const props = defineProps({
-  label: {
-    type: String,
-    required: true
+export default {
+  components: {
+    UIcon
   },
-  icon: {
-    type: String,
-    required: false
+  props: {
+    label: {
+      type: String,
+      required: false
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+    actionOnClick: {
+      type: Function,
+      required: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'primary',
+      validator: function (value) {
+        return ["primary", "warning", "danger"].indexOf(value) !== -1
+      }
+    }
   },
-  actionOnClick: {
-    type: Function,
-    required: true
+  computed: {
+    classes() {
+      return {
+        "u-button": true,
+        [`u-button--${this.type}`]: true
+      }
+    }
   }
-})
+}
+
 </script>
