225121761156875487643d328fe04696846ce94a
Jabuf <jason.buffet@gmail.com>
2022-05-30 15:34:32 +0200

    component: remove the border

--- src/components/basic/USwitch.vue
+++ src/components/basic/USwitch.vue
@@ -3,7 +3,7 @@
     <label for="toggle" class="text-xs font-medium text-gray-900">{{ label }}</label>
     <input type="checkbox" name="toggle" id="toggle"
            :checked="modelValue"
-           class="absolute block w-6 h-6 rounded-full appearance-none cursor-pointer text-indigo-700 focus:ring-0 focus:ring-offset-0"
+           class="absolute block w-6 h-6 rounded-full appearance-none cursor-pointer text-indigo-700 border-transparent focus:ring-0 focus:ring-offset-0"
            v-bind:class="{'right-0': modelValue}"
            @input="(event) => $emit('update:modelValue', event.target.checked)"/>
     <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
