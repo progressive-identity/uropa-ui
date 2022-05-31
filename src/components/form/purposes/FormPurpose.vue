a6313f5513e4b745a86613dc4dc44d787993d4b4
Jabuf <jason.buffet@gmail.com>
2022-05-31 11:10:51 +0200

    component: update files related to purposes

--- src/components/form/purposes/FormPurpose.vue
+++ src/components/form/purposes/FormPurpose.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+  <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5" v-if="state.visible">
     <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-      <div>
-        <h3 class="text-lg leading-6 font-medium text-gray-900">Purpose</h3>
-        <p class="mt-2 text-sm text-gray-700">The purpose of the processing describes what the reason why
-          the controller processes the personal data.</p>
-      </div>
       <div>
         <USwitch v-model="purpose.isMain" label="main"/>
       </div>
@@ -18,10 +13,10 @@
         <div class="pb-2">
           <FormLegalBasis :legal-basis="purpose.legalBasis"/>
         </div>
-        <button type="button" v-on:click="savePurpose"
-                class="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-white font-medium rounded-md text-gray-700 bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          Save
-        </button>
+        <div class="space-x-2">
+          <UButton label="Back" v-on:click="closePurpose" type="secondary"/>
+          <UButton label="Save" v-on:click="savePurpose"/>
+        </div>
       </div>
     </div>
   </div>
@@ -30,9 +25,12 @@
 <script setup>
 import {useStoreData} from '@/store/data.js'
 import {useStoreForms} from '@/store/forms.js'
+import UButton from '@/components/basic/UButton.vue'
 import UInput from '@/components/basic/UInput.vue'
+import {reactive} from 'vue'
 import USwitch from '@/components/basic/USwitch.vue'
 import FormLegalBasis from '@/components/form/purposes/FormLegalBasis.vue'
+import LegalPersonTemplate from '@/data/template/LegalPersonTemplate.json'
 
 const storeData = useStoreData()
 const storeForms = useStoreForms()
@@ -41,23 +39,34 @@ const props = defineProps({
   purpose: {
     type: Object,
     required: true
+  },
+  edition: {
+    type: Boolean,
+    required: false,
+    default: false
   }
 })
 
+const state = reactive({visible: true})
+
 function emptyPurpose() {
   props.purpose.name = ''
-  props.purpose.description = ''
-  props.purpose.legalBasis = {type: '', description: '', proofUrl: ''}
+  props.purpose.organizationStatus = ''
+  props.purpose.legalPerson = LegalPersonTemplate
+  props.purpose.dataCategoriesDisclosed = []
 }
 
-const savePurpose = async () => {
-  storeData.$patch((state) =>
-      state.processingRecord.purposes.push({...props.purpose})
-  )
-  emptyPurpose()
-  storeForms.$patch({
-    purposeVisible: false
-  })
+function savePurpose() {
+  state.visible = false
+  if (!props.edition) {
+    storeData.$patch((state) =>
+        state.processingRecord.purposes.push({...props.purpose}))
+    emptyPurpose()
+  }
+}
+
+function closePurpose() {
+  state.visible = false
 }
 
 </script>
--- src/components/grid/GridPurposes.vue
+++ src/components/grid/GridPurposes.vue
@@ -1,75 +1,64 @@
 <template>
-  <div v-if="current === 2" class="space-y-8 divide-y divide-gray-200">
-    <div>
-      <div>
-        <UButton label="New purpose" :action-on-click="displayPurpose" :icon="mdiPlusCircle"/>
-      </div>
-      <div>
-        <FormSinglePurpose v-if="purposeVisible" :purpose="purposeTemplate"/>
-      </div>
-      <div class="mt-8 flex flex-col">
-        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-300">
-                <thead class="bg-gray-50">
-                <tr>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Description</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Legal basis type
-                  </th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Main</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                    <span class="sr-only">Edit</span>
-                  </th>
-                </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                <tr v-for="purpose in purposes" :key="purpose.name">
-                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                    {{ purpose.name }}
-                  </td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ purpose.description }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ purpose.legalBasis.type }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                    <UIcon v-if="purpose.isMain" :path="mdiCheck"/>
-                  </td>
-                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                    >Edit<span class="sr-only">, {{  }}</span></a
-                    >
-                  </td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
+  <div v-if="current === 2">
+    <UButton label="New purpose" v-on:click="createPurpose" :icon="mdiPlusCircle"/>
+    <div class=" py-5">
+      <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="(purpose, index) in processingRecord.purposes" :key="index"
+            class="bg-white rounded-lg shadow border-2 px-5 text-sm">
+          <div class="h-1/4 align-top text-gray-900 font-medium columns-2 py-5">
+            <p class="w-full truncate">{{ purpose.name }}</p>
+            <p class="w-full text-right" v-if="purpose.isMain">main</p>
           </div>
-        </div>
-      </div>
+          <div class="h-2/4">
+            <p class="mt-1 text-gray-500">{{ purpose.description }}</p>
+          </div>
+          <div class="h-1/4 align-top text-gray-900 font-medium py-5">
+          </div>
+          <div class="h-1/4 py-5 space-x-2 align-bottom">
+            <UButton v-on:click="editPurpose(purpose)" :icon="mdiPencil"/>
+            <UButton v-on:click="deletePurpose(index)" :icon="mdiDelete" type="danger"/>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div v-if="state.purposeVisible">
+      <FormPurpose :purpose="state.purpose" :edition="state.edition"/>
     </div>
   </div>
 </template>
 
 <script setup>
+import {reactive} from 'vue'
 import {useStore} from '@/store/stepper.js'
 import {useStoreData} from '@/store/data.js'
-import {useStoreForms} from '@/store/forms.js'
 import {storeToRefs} from 'pinia'
-import FormSinglePurpose from '@/components/form/purposes/FormPurpose.vue'
 import UButton from '@/components/basic/UButton.vue'
-import UIcon from '@/components/basic/UIcon.vue'
-import {mdiCheck, mdiPlusCircle} from '@mdi/js'
+import FormPurpose from '@/components/form/purposes/FormPurpose.vue'
+import {mdiDelete, mdiPencil, mdiPlusCircle} from '@mdi/js'
+import PurposeTemplate from '../../data/template/PurposeTemplate.json'
 
 const store = useStore()
 const {current} = storeToRefs(store)
-const {processingRecord: {purposes}, purposeTemplate} = useStoreData()
+const storeData = useStoreData()
+const {processingRecord} = storeToRefs(storeData)
+const state = reactive({purpose: PurposeTemplate, edition: false, purposeVisible: false})
 
-const storeForms = useStoreForms()
-const {purposeVisible} = storeToRefs(storeForms)
 
-const displayPurpose = async () => {
-  storeForms.$patch({
-    purposeVisible: !storeForms.purposeVisible
+function createPurpose() {
+  state.purpose = PurposeTemplate
+  state.purposeVisible = true
+  state.edition = false
+}
+
+function editPurpose(purpose) {
+  state.purpose = purpose
+  state.purposeVisible = true
+  state.edition = true
+}
+
+function deletePurpose(index) {
+  storeData.$patch((state) => {
+    state.processingRecord.purposes.splice(index, 1)
   })
 }
 
