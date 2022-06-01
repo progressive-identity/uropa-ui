699267d125263eea987ff8f0dde7f7f263b9efe2
Jabuf <jason.buffet@gmail.com>
2022-06-01 12:35:05 +0200

    component: improve the switch component

--- src/components/basic/USwitch.vue
+++ src/components/basic/USwitch.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-    <label for="toggle" class="text-xs font-medium text-gray-900">{{ label }}</label>
-    <input type="checkbox" name="toggle" id="toggle"
-           :checked="modelValue"
-           class="absolute block w-6 h-6 rounded-full appearance-none cursor-pointer text-indigo-700 border-transparent focus:ring-0 focus:ring-offset-0"
-           v-bind:class="{'right-0': modelValue}"
-           @input="(event) => $emit('update:modelValue', event.target.checked)"/>
-    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+  <div>
+    <span class="text-gray-900 align-top pr-3" v-if="label">{{ label }}</span>
+    <Switch v-model="modelValue" :onChange="emit('update:modelValue', modelValue)"
+            :class="[modelValue ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
+    <span aria-hidden="true"
+          :class="[modelValue ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']"/>
+    </Switch>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    label: {
-      type: String,
-      required: false
-    },
-    modelValue: {
-      type: Boolean,
-      required: true
-    },
-    emits: ['update:modelValue']
+
+<script setup>
+import {storeToRefs} from 'pinia'
+import {Switch} from '@headlessui/vue'
+import {useStoreDisplay} from '@/store/display.js'
+
+const storeDisplay = useStoreDisplay()
+const {formsDisplayed} = storeToRefs(storeDisplay)
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: false
+  },
+  modelValue: {
+    type: Boolean,
+    required: true
   }
-}
+})
+
+const emit = defineEmits(['update:modelValue'])
 </script>
