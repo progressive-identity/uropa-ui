98e60d69d6823a2f71c6d95ea9d8c17b2085fc22
Jabuf <jason.buffet@gmail.com>
2022-06-01 23:16:23 +0200

    clean: add a missing input and remove useless classes

--- src/components/form/legal-person/FormCeo.vue
+++ src/components/form/legal-person/FormCeo.vue
@@ -4,7 +4,9 @@
       <h3 class="text-lg leading-6 font-medium text-gray-900">CEO</h3>
       <p class="mt-1 max-w-2xl text-sm text-gray-500">The highest-ranking person of the legal person.</p>
     </div>
-    <div class="space-y-8 divide-y divide-gray-200">
+    <div class="space-y-3">
+        <UInput v-model="ceo.professionalTitle" label="Professional title"
+                :rounded-top-left="true" :rounded-top-right="true" :rounded-bottom-right="true" :rounded-bottom-left="true"/>
       <FormPersonalInformation :personal-information="ceo.personalInformation"/>
     </div>
   </div>
@@ -13,6 +15,7 @@
 <script setup>
 import {storeToRefs} from 'pinia'
 import {useStoreDisplay} from '@/store/display.js'
+import UInput from '@/components/basic/UInput.vue'
 import FormPersonalInformation from '@/components/form/FormPersonalInformation.vue'
 
 const storeDisplay = useStoreDisplay()
--- src/components/form/legal-person/FormDpo.vue
+++ src/components/form/legal-person/FormDpo.vue
@@ -4,7 +4,7 @@
       <h3 class="text-lg leading-6 font-medium text-gray-900">DPO</h3>
       <p class="mt-1 max-w-2xl text-sm text-gray-500">The Data Protection Officer (DPO) of the legal person.</p>
     </div>
-    <div class="space-y-3 divide-y divide-gray-200">
+    <div class="space-y-3">
       <FormPersonalInformation :personal-information="dpo.personalInformation"/>
       <FormPostalAddress :postal-address="dpo.postalAddress"/>
     </div>
