0a3b3282dbfea7a69ec32768a7d1c11a3631750a
Jabuf <jason.buffet@gmail.com>
2022-06-01 12:45:33 +0200

    component: improving the label

--- src/components/basic/USwitch.vue
+++ src/components/basic/USwitch.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <span class="text-gray-900 align-top pr-3" v-if="label">{{ label }}</span>
+  <SwitchGroup as="div" class="flex items-center">
+    <SwitchLabel v-if="label" as="span" class="mr-3">
+      <span class="text-sm font-medium text-gray-900">{{ label }}</span>
+    </SwitchLabel>
     <Switch v-model="modelValue" :onChange="emit('update:modelValue', modelValue)"
             :class="[modelValue ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
     <span aria-hidden="true"
           :class="[modelValue ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']"/>
     </Switch>
-  </div>
+  </SwitchGroup>
 </template>
 
 <script setup>
 import {storeToRefs} from 'pinia'
-import {Switch} from '@headlessui/vue'
+import {Switch, SwitchGroup, SwitchLabel} from '@headlessui/vue'
 import {useStoreDisplay} from '@/store/display.js'
 
 const storeDisplay = useStoreDisplay()
