fbd993459a8c9fa4fa1fef78569528e326e55d34
Jabuf <jason.buffet@gmail.com>
2022-06-09 15:05:01 +0200

    component: improve the grid for joint controllers

--- src/components/grid/GridJointControllers.vue
+++ src/components/grid/GridJointControllers.vue
@@ -4,10 +4,24 @@
     <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       <li v-for="(jointController, index) in processingRecord.jointControllers" :key="index"
           class="bg-white rounded-lg shadow border-2 px-5 text-sm">
-        <div class="h-1/4 align-top text-gray-900 font-medium py-5">
-          <p class="w-full truncate">{{ jointController.legalPerson.name }}</p>
+        <div class="px-4 py-3 sm:px-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">{{ jointController.legalPerson.name }}</h3>
         </div>
-        <div class="h-1/4 py-5 space-x-2 align-bottom">
+        <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <dl v-if="jointController.legalPerson.dpo.personalInformation.firstName" class="sm:divide-y sm:divide-gray-200">
+            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">DPO</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ jointController.legalPerson.dpo.personalInformation.lastName.toUpperCase() + ' ' + jointController.legalPerson.dpo.personalInformation.firstName }}</dd>
+            </div>
+          </dl>
+          <dl v-if="jointController.legalPerson.ceo.personalInformation.firstName" class="sm:divide-y sm:divide-gray-200">
+            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">CEO</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ jointController.legalPerson.ceo.personalInformation.lastName.toUpperCase() + ' ' + jointController.legalPerson.ceo.personalInformation.firstName }}</dd>
+            </div>
+          </dl>
+        </div>
+        <div class="py-5 space-x-2 align-bottom">
           <UButton v-on:click="editJointController(jointController)" :icon="mdiPencil"/>
           <UButton v-on:click="deleteJointController(index)" :icon="mdiDelete" type="danger"/>
         </div>
