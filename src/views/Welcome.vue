7ac43884a03e0d0302851344f1f5c42ca07789cc
Jabuf <jason.buffet@gmail.com>
2022-06-10 18:06:00 +0200

    component: use the file picker to load existing data

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,13 +3,14 @@
     <div class="flex w-full items-center justify-center h-screen">
       <div class="max-w-2xl mx-auto space-y-5">
         <div class="flex justify-center">
-          <UButton type="primary" label="Start a new processing record" @click="startProcessingRecord"/>
+          <UButton type="primary" :icon="mdiClipboardEditOutline"
+                   label="Start a new processing record" @click="startProcessingRecord"/>
         </div>
-        <div class="flex justify-center">
+        <div class="flex justify-center text-xl font-medium text-gray-700">
           OR
         </div>
         <div class="flex justify-center">
-          <UButton type="primary" label="Import an existing one"/>
+          <UFilePicker label="Import an existing one" accept=".json" :onUpload="startProcessingRecord"/>
         </div>
       </div>
     </div>
@@ -19,15 +20,25 @@
 <script setup>
 import {storeToRefs} from 'pinia'
 import {useStore} from '@/store/stepper'
+import {useStoreData} from '@/store/data.js'
 import UButton from '@/components/basic/UButton.vue'
+import UFilePicker from '@/components/basic/UFilePicker.vue'
+import {mdiClipboardEditOutline} from '@mdi/js'
 
 const store = useStore()
 const {current} = storeToRefs(store)
+const storeData = useStoreData()
+const {processingRecord} = storeToRefs(storeData)
 
-function startProcessingRecord() {
+function startProcessingRecord(data = null) {
   store.current++
   store.$patch({
     mainNavigationDisplayed: true
   })
+  if (data) {
+    storeData.$patch({
+      processingRecord: JSON.parse(data)
+    })
+  }
 }
 </script>
