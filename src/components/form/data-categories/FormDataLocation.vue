62ef1264a15de952122e901ff15342c9a80cc7d2
Jabuf <jason.buffet@gmail.com>
2022-06-13 16:46:15 +0200

    component: update the visual of the data location form

--- src/components/form/data-categories/FormDataLocation.vue
+++ src/components/form/data-categories/FormDataLocation.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5" v-if="formsDisplayed.dataLocation">
-    <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-      <div>
-        <h3 class="text-lg leading-6 font-medium text-gray-900">Data location</h3>
-        <p class="mt-2 text-sm text-gray-700">Place where a data item is stored within a DataSource.</p>
-      </div>
-      <div class="isolate -space-y-px rounded-md shadow-sm">
-        <div class="py-2">
-          <UInput v-model="dataLocation.description" label="Description" />
-          <USelectEnums v-model="dataLocation.dataSupport" label="Data support" :list="dataSupports"
-                        />
-          <USelectEnums v-model="dataLocation.storageState" label="Storage state" :list="storageStates"
-                        />
-        </div>
-        <FormDataSource :data-source="dataLocation.dataSource"/>
-        <UMultiSelect v-model="dataLocation.dataTypes" label="Data types stored"
-                      :list="storeData.dataTypesForDataLocation"/>
-        <UInput v-for="dataType in dataLocation.dataTypes" v-model="dataType.path"
-                :label="`Path for ${dataType.name}`"/>
-        <div class="space-x-2 py-3">
-          <UButton label="Back" v-on:click="closeDataLocation" type="secondary"/>
-          <UButton label="Save" v-on:click="saveDataLocation"/>
-        </div>
-      </div>
+  <div v-if="formsDisplayed.dataLocation">
+    <div>
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Data location</h3>
+      <p class="mt-2 text-sm text-gray-700">Place where a data item is stored within a DataSource.</p>
+    </div>
+    <div class="pt-3">
+      <UInput v-model="dataLocation.description" label="Description" size="xl"/>
+      <USelectEnums v-model="dataLocation.dataSupport" label="Data support" :list="dataSupports"
+      />
+      <USelectEnums v-model="dataLocation.storageState" label="Storage state" :list="storageStates"
+      />
+    </div>
+    <FormDataSource :data-source="dataLocation.dataSource"/>
+    <UMultiSelect v-model="dataLocation.dataTypes" label="Data types stored"
+                  :list="storeData.dataTypesForDataLocation"/>
+    <UInput v-for="dataType in dataLocation.dataTypes" v-model="dataType.path"
+            :label="`Path for ${dataType.name}`" size="xl"/>
+    <div class="space-x-2 py-3">
+      <UButton label="Back" v-on:click="closeDataLocation" type="secondary"/>
+      <UButton label="Save" v-on:click="saveDataLocation"/>
     </div>
   </div>
 </template>
--- src/components/form/data-categories/FormDataSource.vue
+++ src/components/form/data-categories/FormDataSource.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-    <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-      <div>
-        <h3 class="text-lg leading-6 font-medium text-gray-900">Data source</h3>
-        <p class="mt-2 text-sm text-gray-700">Abstraction of the medium on which Personal data is stored. It could be a
-          hard drive on site, a SaaS, or even an archive room.</p>
-      </div>
-      <div class="isolate -space-y-px rounded-md shadow-sm">
-        <div class="columns-2 gap-0 py-2">
-          <UInput v-model="dataSource.name" label="Name" placeholder="The name of the data source (ex: name of database, server etc.)" />
-          <UInput v-model="dataSource.storageType" label="Storage type" placeholder="Provides useful information on the storage type" />
-        </div>
-        <USelect v-model="dataSource.country" :list="countries" label="Country"
-                 />
-      </div>
+  <div class="pt-8 space-y-6">
+    <div>
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Data source</h3>
+      <p class="mt-2 text-sm text-gray-700">Abstraction of the medium on which Personal data is stored. It could be a
+        hard drive on site, a SaaS, or even an archive room.</p>
+    </div>
+    <USelect v-model="dataSource.country" :list="countries" label="Country" size="s"/>
+    <div class="flex flex-wrap">
+      <UInput v-model="dataSource.name" label="Name"
+              placeholder="The name of the data source (ex: name of database, server etc.)" class="flex-1"/>
+      <UInput v-model="dataSource.storageType" label="Storage type"
+              placeholder="Provides useful information on the storage type" class="flex-1"/>
+      <div class="grow"></div>
     </div>
   </div>
 </template>
