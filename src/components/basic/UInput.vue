1735610773319c81566568f0b650aeef4e4d9b74
Jabuf <jason.buffet@gmail.com>
2022-06-13 15:33:54 +0200

    css: add classes for the w-size of the input

--- src/components/basic/UInput.vue
+++ src/components/basic/UInput.vue
@@ -3,41 +3,51 @@
     <label for="name" class="u-label">{{ label }}
       <span v-if="required"> *</span>
     </label>
-    <input type="text" :value="modelValue"
+    <input :type="type" :value="modelValue"
            @input="$emit('update:modelValue', $event.target.value)"
-           class="u-input"
-           :class="'max-w-'+size"
+           :class="classes"
            :placeholder="placeholder"/>
     <p v-if="required" class="mt-1 text-sm text-red-600" id="email-error">This field is mandatory.</p>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    label: {
-      type: String,
-      required: true
-    },
-    modelValue: {
-      type: String,
-      required: true
-    },
-    placeholder: {
-      type: String,
-      required: false
-    },
-    required: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    size: {
-      type: String,
-      required: false,
-      default: 'xl'
-    }
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: false,
+    default: 'text'
   },
-  emits: ['update:modelValue']
-}
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    required: false
+  },
+  required: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  size: {
+    type: String,
+    required: false,
+    default: 'l'
+  }
+})
+
+const classes = computed(() => {
+  return {
+    'u-input': true,
+    [`u-input--${props.size}`]: true
+  }
+})
 </script>
