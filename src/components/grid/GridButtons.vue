7a67da60f1a3dd8ac68cda4b40b0daeab854edc9
Jabuf <jason.buffet@gmail.com>
2022-06-20 14:38:57 +0200

    component: fix an issue with the emits

--- src/components/grid/GridButtons.vue
+++ src/components/grid/GridButtons.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex space-x-1 right-0 top-2 absolute">
-    <UButton @click="this.$emit('edit')" :icon="mdiPencil"/>
-    <UButton @click="this.$emit('delete')" :icon="mdiDelete" type="danger"/>
+    <UButton @click="emits('edit')" :icon="mdiPencil"/>
+    <UButton @click="emits('delete')" :icon="mdiDelete" type="danger"/>
   </div>
 </template>
 
 <script setup>
 import UButton from '@/components/basic/UButton.vue'
-import {mdiDelete, mdiPencil} from '@mdi/js'</script>
+import {mdiDelete, mdiPencil} from '@mdi/js'
+
+const emits = defineEmits(['edit', 'delete'])
+
+</script>
