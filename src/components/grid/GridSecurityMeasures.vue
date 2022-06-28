02dc03de94b152bfd7da2f807ba208881283adf0
Jabuf <jason.buffet@gmail.com>
2022-06-28 16:32:09 +0200

    ur-45: update the grid of security measure

--- src/components/grid/GridSecurityMeasures.vue
+++ src/components/grid/GridSecurityMeasures.vue
@@ -6,16 +6,34 @@
           class="u-grid">
         <div class="relative px-4 py-5">
           <div class="flex items-center">
-            <h3>{{ securityMeasure.name }}</h3>
+            <h3>
+              <UIcon size="24" :path="mdiSecurity"/>
+              {{ securityMeasure.name }}
+            </h3>
           </div>
           <GridButtons @edit="editSecurityMeasure(securityMeasure)" @delete="deleteSecurityMeasure(index)"/>
         </div>
         <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl class="sm:divide-y sm:divide-gray-200">
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-500">Type</dt>
+              <dt class="flex items-center text-sm font-medium text-gray-500">
+                <UIcon :path="mdiLock"/>
+                Type
+              </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{
                   securityMeasure.securityMeasureType
+                }}
+              </dd>
+            </div>
+          </dl>
+          <dl class="sm:divide-y sm:divide-gray-200">
+            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="flex items-center text-sm font-medium text-gray-500">
+                <UIcon :path="mdiText"/>
+                Description
+              </dt>
+              <dd class="flex items-center mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{
+                  securityMeasure.description
                 }}
               </dd>
             </div>
@@ -36,9 +54,10 @@ import {useStoreDisplay} from '@/store/display.js'
 import {storeToRefs} from 'pinia'
 import UButton from '@/components/basic/UButton.vue'
 import FormSecurityMeasure from '@/components/form/security-measures/FormSecurityMeasure.vue'
-import {mdiDelete, mdiPencil, mdiPlusCircle} from '@mdi/js'
+import {mdiLock, mdiPlusCircle, mdiSecurity, mdiText} from '@mdi/js'
 import SecurityMeasureTemplate from '../../data/template/SecurityMeasureTemplate.json'
 import GridButtons from '@/components/grid/GridButtons.vue'
+import UIcon from '@/components/basic/UIcon.vue'
 
 const storeData = useStoreData()
 const {processingRecord} = storeToRefs(storeData)
