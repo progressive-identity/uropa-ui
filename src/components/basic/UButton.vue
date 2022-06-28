e5f7550c4074c5f44641e5a9fb9e55e55cbee6b5
Jabuf <jason.buffet@gmail.com>
2022-06-28 16:57:15 +0200

    clean: switch to composition api

--- src/components/basic/UButton.vue
+++ src/components/basic/UButton.vue
@@ -6,40 +6,35 @@
   </button>
 </template>
 
-<script>
+<script setup>
+import {computed} from 'vue'
 import UIcon from '@/components/basic/UIcon.vue'
-// TODO change to defineProps
-export default {
-  components: {
-    UIcon
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: false
   },
-  props: {
-    label: {
-      type: String,
-      required: false
-    },
-    icon: {
-      type: String,
-      required: false
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'primary',
-      validator: function (value) {
-        return ['primary', 'secondary', 'warning', 'danger'].indexOf(value) !== -1
-      }
-    }
+  icon: {
+    type: String,
+    required: false
   },
-  computed: {
-    classes() {
-      return {
-        'u-button': true,
-        [`u-button--${this.type}`]: true,
-        ['u-button--icon']: (this.icon && !this.label)
-      }
+  type: {
+    type: String,
+    required: false,
+    default: 'primary',
+    validator: function (value) {
+      return ['primary', 'secondary', 'warning', 'danger'].indexOf(value) !== -1
     }
   }
-}
+})
+
+const classes = computed(() => {
+  return {
+    'u-button': true,
+    [`u-button--${props.type}`]: true,
+    ['u-button--icon']: (props.icon && !props.label)
+  }
+})
 
 </script>
