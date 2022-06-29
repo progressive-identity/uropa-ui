ac62127ad8f0f5803ba307178de6b951699e2c19
Jabuf <jason.buffet@gmail.com>
2022-06-29 12:25:23 +0200

    ur-24: rework the footer

--- src/components/UropaFooter.vue
+++ src/components/UropaFooter.vue
@@ -1,14 +1,20 @@
 <template>
-  <footer class="bg-white p-4">
-    <div class="max-w-7xl mx-auto px-4">
-      <div class="flex justify-center space-x-6 md:order-2">
-        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
-        <UIcon
-          :path="item.icon" :size="24"/>
+  <footer class="p-4 text-gray-400">
+    <div class="max-w-7xl mx-auto space-y-4">
+      <div class="flex justify-center space-x-12">
+        <a v-for="link in legalLinks" :key="link.name" :href="link.href" class="hover:text-gray-500">
+          {{ link.name }}
         </a>
       </div>
-      <div class="mt-8 md:mt-0 md:order-1">
-        <p class="text-center text-base text-gray-400">&copy; 2022 Alias.dev</p>
+      <div class="flex justify-center space-x-6">
+        <a v-for="link in externalLinks" :key="link.name" :href="link.href" class="hover:text-gray-500">
+          <UIcon
+              :path="link.icon" :size="24"/>
+        </a>
+      </div>
+      <div>
+        <!-- TODO see with Aurelien what to put here-->
+        <p class="text-center text-base">&copy; 2022 CODE IS LAW.</p>
       </div>
     </div>
   </footer>
@@ -16,23 +22,44 @@
 
 <script setup>
 import UIcon from '@/components/basic/UIcon.vue'
-import { mdiGithub, mdiTwitter, mdiLinkedin } from '@mdi/js'
+import {mdiGithub, mdiLinkedin, mdiTwitter, mdiWeb} from '@mdi/js'
 
-const navigation = [
+const externalLinks = [
+  {
+    name: 'Alias.dev',
+    href: process.env.VUE_APP_URL_ALIAS_DEV,
+    icon: mdiWeb
+  },
+  {
+    name: 'Linkedin',
+    href: process.env.VUE_APP_URL_LINKEDIN,
+    icon: mdiLinkedin
+  },
   {
     name: 'Twitter',
-    href: '#',
+    href: process.env.VUE_APP_URL_TWITTER,
     icon: mdiTwitter
   },
   {
     name: 'GitHub',
-    href: '#',
+    href: process.env.VUE_APP_URL_GITHUB_UROPA,
     icon: mdiGithub
+  }
+]
+const legalLinks = [
+//TODO see with Aurelien what to put in there
+//TODO see with Jules what's the best way to handle these pages in our SPA
+  {
+    name: 'Privacy policy',
+    href: '#'
   },
   {
-    name: 'Linkedin',
-    href: '#',
-    icon: mdiLinkedin
+    name: 'Legal Notice',
+    href: '#'
+  },
+  {
+    name: 'Terms of Services',
+    href: '#'
   }
 ]
 </script>
