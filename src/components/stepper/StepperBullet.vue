c38bec56f161ba271ad6edf84962b98d57c10f9a
Jabuf <jason.buffet@gmail.com>
2022-07-01 10:00:49 +0200

    ur-13: add a button to skip optional steps

--- src/components/stepper/StepperBullet.vue
+++ src/components/stepper/StepperBullet.vue
@@ -5,18 +5,21 @@
       <UButton type="secondary" v-on:click="emit('update:modelValue', modelValue-1)" v-if="modelValue > 0"
                label="Previous"/>
       <UButton type="primary" v-on:click="next" label="Next"/>
+      <UButton v-if="steps.find(e => e.id === modelValue).optional" type="primary" v-on:click="skip" label="Skip"/>
       <DownloadButton/>
     </div>
   </div>
   <nav aria-label="Progress" class="flex items-center justify-center pt-5">
-    <ol class="ml-8 flex items-center space-x-5" role="list">
+    <ol class="ml-8 flex space-x-5" role="list">
       <li v-for="step in steps" :key="step.name">
         <div v-if="step.id < modelValue">
           <p class="block rounded-full relative flex items-center justify-center">
             <span aria-hidden="true"
                   class="relative hover:bg-primary-700 block w-2.5 h-2.5 bg-primary-500 rounded-full"/>
           </p>
-          <p class="py-2 text-sm font-medium text-gray-900">{{ step.name }}</p>
+          <p class="py-2 text-sm font-medium text-gray-900">{{ step.name }}
+            <br/><span v-if="step.optional" class="flex justify-center font-light italic">(optional)</span>
+          </p>
         </div>
         <div v-else-if="step.id === modelValue" aria-current="step">
           <p class="relative flex items-center justify-center">
@@ -25,13 +28,17 @@
           </span>
             <span aria-hidden="true" class="relative block w-2.5 h-2.5 bg-primary-500 rounded-full"/>
           </p>
-          <p class="py-2 text-sm font-medium text-primary-500">{{ step.name }}</p>
+          <p class="py-2 text-sm font-medium text-primary-500">{{ step.name }}
+            <br/><span v-if="step.optional" class="flex justify-center font-light italic">(optional)</span>
+          </p>
         </div>
         <div v-else>
           <p class="block rounded-full relative flex items-center justify-center">
             <span aria-hidden="true" class="relative hover:bg-gray-500 block w-2.5 h-2.5 bg-gray-300 rounded-full"/>
           </p>
-          <p class="py-2 text-sm font-medium text-gray-500">{{ step.name }}</p>
+          <p class="py-2 text-sm font-medium text-gray-500">{{ step.name }}
+            <br/><span v-if="step.optional" class="flex justify-center font-light italic">(optional)</span>
+          </p>
         </div>
       </li>
     </ol>
@@ -78,6 +85,19 @@ async function next() {
     } else {
       emit('update:modelValue', props.modelValue + 1)
     }
+  }
+}
+
+
+async function skip() {
+  storeDisplay.$reset()
+  if (props.modelValue === props.steps.length - 1) {
+    store.current++
+    store.$patch({
+      mainNavigationDisplayed: true
+    })
+  } else {
+    emit('update:modelValue', props.modelValue + 1)
   }
 }
 
--- src/store/stepper.js
+++ src/store/stepper.js
@@ -1,36 +1,39 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia'
 
 export const useStore = defineStore('stepper', {
-    state: () => {
-        return {
-            mainNavigationDisplayed: false,
-            stepsProcessingRecord: [
-                {id: 0, name: 'General information'},
-                {id: 1, name: 'Data controllers'},
-                {id: 2, name: 'Purposes'},
-                {id: 3, name: 'Data categories'},
-                {id: 4, name: 'Recipients'},
-                {id: 5, name: 'Security measures'},
-                {id: 6, name: 'Data transfers'}
-            ],
-            current: -1,
-            stepsDataCategories: [
-                {id: 0, name: 'Data categories'},
-                {id: 1, name: 'Data sources'}
-            ],
-            currentDataCategories: 0,
-            stepsDataControllers: [
-                {id: 0, name: 'Data controller'},
-                {id: 1, name: 'Representative'},
-                {id: 2, name: 'Joint controllers'}
-            ],
-            currentDataControllers: 0,
-            stepsRecipients: [
-                {id: 0, name: 'Internal departments'},
-                {id: 1, name: 'Data processors'},
-                {id: 2, name: 'External organizations'}
-            ],
-            currentRecipients: 0
-        }
+  state: () => {
+    return {
+      mainNavigationDisplayed: false,
+      stepsProcessingRecord: [
+        { id: 0, name: 'General information' },
+        { id: 1, name: 'Data controllers' },
+        {
+          id: 2,
+          name: 'Purposes'
+        },
+        { id: 3, name: 'Data categories' },
+        { id: 4, name: 'Recipients' },
+        { id: 5, name: 'Security measures' },
+        { id: 6, name: 'Data transfers' }
+      ],
+      current: -1,
+      stepsDataCategories: [
+        { id: 0, name: 'Data categories' },
+        { id: 1, name: 'Data sources' }
+      ],
+      currentDataCategories: 0,
+      stepsDataControllers: [
+        { id: 0, name: 'Data controller', optional: false },
+        { id: 1, name: 'Representative', optional: true },
+        { id: 2, name: 'Joint controllers', optional: true }
+      ],
+      currentDataControllers: 0,
+      stepsRecipients: [
+        { id: 0, name: 'Internal departments', optional: true },
+        { id: 1, name: 'Data processors', optional: true },
+        { id: 2, name: 'External organizations', optional: true }
+      ],
+      currentRecipients: 0
     }
+  }
 })
