f8db4a5457fef65d66f3c2aaa359b69c79b7f96d
Jabuf <jason.buffet@gmail.com>
2022-07-01 15:31:33 +0200

    ur-15: add the last missing descriptions

--- src/data/enums.js
+++ src/data/enums.js
@@ -1,85 +1,125 @@
-const collectionMeans = [{
+const collectionMeans = [
+  {
     name: 'inferred',
-    description: 'Personal data that is derived data generated from existing data'
-}, {
+    description:
+      'Personal data that is derived data generated from existing data'
+  },
+  {
     name: 'generated',
-    description: 'Personal data that is generated without relation from existing data'
-}, {
+    description:
+      'Personal data that is generated without relation from existing data'
+  },
+  {
     name: 'collected',
     description: 'Personal data that has been collected'
-}]
+  }
+]
 
-const securityMeasureTypes = [{
+const securityMeasureTypes = [
+  {
     name: 'organizational'
-}, {
+  },
+  {
     name: 'physical'
-}, {
+  },
+  {
     name: 'technical'
-}]
+  }
+]
 
-const dataSupports = [{
+const dataSupports = [
+  {
     name: 'digital'
-}, {
+  },
+  {
     name: 'physical'
-}]
-const storageStates = [{
+  }
+]
+const storageStates = [
+  {
     name: 'production',
     description: 'The data is kept in the active database'
-}, {
+  },
+  {
     name: 'legal',
     description: 'The data is kept for legal reasons in a restricted database'
-}, {
+  },
+  {
     name: 'data locking',
-    description: 'The data is locked for legal reasons (legal obligation only for Spain)'
-}, {
+    description:
+      'The data is locked for legal reasons (legal obligation only for Spain)'
+  },
+  {
     name: 'archives',
-    description: 'The data is archived with no time limitation (only for archival purposes in the public interest)'
-}]
+    description:
+      'The data is archived with no time limitation (only for archival purposes in the public interest)'
+  }
+]
 
-const legalBasisTypes = [{
+const legalBasisTypes = [
+  {
     name: 'consent'
-}, {
+  },
+  {
     name: 'contract'
-}, {
+  },
+  {
     name: 'legal obligation'
-}, {
+  },
+  {
     name: 'vital interest'
-}, {
+  },
+  {
     name: 'public interest task'
-}, {
+  },
+  {
     name: 'legitimate interest'
+  }
+]
 
-}]
-
-const dataTransferLegalBasisTypes = [{
+const dataTransferLegalBasisTypes = [
+  {
     name: 'adequacy decision',
-    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit'
-}, {
+    description:
+      'The Commission has decided that the third country in question ensures an adequate level of protection (art. 45 GDPR)'
+  },
+  {
     name: 'appropriate safeguards',
-    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium'
-}, {
+    description:
+      'Legally binding and enforceable agreement, BCR, SCC, approved code of conduct or approved certification mechanisms (art. 46 GDPR)'
+  },
+  {
     name: 'derogation',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
-}]
+    description:
+      'Explicit consent, contract, public interest, legal claims, protection of vital interests, public register, ad hoc transfer (art. 49 GDPR)'
+  }
+]
 
-const timeUnit = [{
+const timeUnit = [
+  {
     name: 'years'
-}, {
+  },
+  {
     name: 'months'
-}, {
+  },
+  {
     name: 'weeks'
-}, {
+  },
+  {
     name: 'days'
-}, {
+  },
+  {
     name: 'hours'
-}, {}]
+  },
+  {}
+]
 
 export {
-    collectionMeans,
-    securityMeasureTypes,
-    dataSupports,
-    storageStates,
-    dataTransferLegalBasisTypes,
-    legalBasisTypes,
-    timeUnit
+  collectionMeans,
+  securityMeasureTypes,
+  dataSupports,
+  storageStates,
+  dataTransferLegalBasisTypes,
+  legalBasisTypes,
+  timeUnit
 }
