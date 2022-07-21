52434a621ee4e9cec06323c1e22b4ce1a8c9c89b
Jabuf <jason.buffet@gmail.com>
2022-07-21 17:41:56 +0200

    ur-67: refactor the way template are managed

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './css/main.css'
 import { createPinia } from 'pinia'
+import { prepareTemplates } from '@/data/prepareTemplates.js'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
+
+prepareTemplates() // TODO add explanation
