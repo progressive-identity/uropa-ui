e88d9c90c4656311f96ea75c8d947f2b33e6a73d
Jabuf <jason.buffet@gmail.com>
2022-07-28 18:28:03 +0200

    ur-97: migrate the file on import and curate the file on download

--- src/components/stepper/DownloadButton.vue
+++ src/components/stepper/DownloadButton.vue
@@ -3,20 +3,15 @@
 </template>
 
 <script setup>
-import {reactive} from 'vue'
-import {storeToRefs} from 'pinia'
 import {useStoreData} from '@/store/data.js'
 import UButton from '@/components/basic/UButton.vue'
-import {getCuratedRopa} from '@/composables/useCuration.js'
 import {saveToJsonFile} from '@/utils/file.js'
 import {mdiDownload} from '@mdi/js'
+import {getCuratedRopa} from '@/composables/useCuration.js'
 
 const storeData = useStoreData()
-const {ropa} = storeToRefs(storeData)
-
-const state = reactive({json: ''})
 
 function download() {
-  saveToJsonFile(getCuratedRopa(ropa.value))
+  saveToJsonFile(getCuratedRopa(storeData.ropa))
 }
 </script>
